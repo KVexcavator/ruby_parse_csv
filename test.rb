0b1f0eb49a9db332b854008e79eb9b4104e069d8
kv-2 <str_token@mail.ru>
2019-05-07 17:24:46 +0300

    intial

---
+++ test.rb
@@ -0,0 +1,19 @@
+#coding: utf-8
+require 'csv'
+File.open("test.csv", "r:utf-8") do |f|
+  p table=CSV.parse(f, headers: true)
+  p table.by_col[1]
+  p table.by_row[0]
+  p table[0]["id"]
+  p table[0]["name"]
+  p table.empty?
+  p table.size
+  p table.length()
+  table.headers.each{|w| p w}
+end
+
+
+
+#table_end= CSV.open("test_end.csv","w:utf-8") # key a to add
+#table_end.close
+#file.truncate(f,0) #to wipe all contain file
