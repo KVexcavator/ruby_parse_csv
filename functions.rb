06b2405f89dba2e716f254f5628b4c10bb3b79b9
kv-2 <str_token@mail.ru>
2019-05-08 21:13:23 +0300

    add data to outer_file

--- functions.rb
+++ functions.rb
@@ -1,9 +1,36 @@
 
-# функция для создания результирующей таблицы
-def add_data_outer  outer,data  
+# функция для добавления результирующей таблицы
+def add_header_data_outer  outer,data  
   # file outer date (key a to add)
   o= CSV.open(outer,"w:utf-8") 
   o << data
   o.close
   return o
+end
+# функция для добавления рядов в результирующгю таблицу
+def add_data_outer  outer,data  
+  # file outer date (key a to add)
+  o= CSV.open(outer,"a:utf-8") 
+  o << data
+  o.close
+  return o
+end
+
+# функция приводит неупорядоченные заголовки к стандартным
+# возвращает новый ряд с заголовками
+def normalize_headers_names head,head_data
+  head.each do|w|
+    # привести нименнования к стандартным
+    w=w.strip.downcase
+    #настройку regex целесобрзно произволить по отдельности
+    w=w.gsub(/№/,"№")
+    w=w.gsub(/^мар.*/,"Марка")
+    w=w.gsub(/^разм.*/,"Размер")
+    w=w.gsub(/^исп.*/,"Исполнение")
+    w=w.gsub(/^напр.*/,"Напряжение")
+    w=w.gsub(/^тех.*/,"ТУ")
+    w=w.gsub(/^гост.*/,"ГОСТ")
+    w=w.gsub(/^цвет.*/,"Цвет")
+    head_data<<w
+  end
 end
--- parser.rb
+++ parser.rb
@@ -10,30 +10,20 @@ loger="loger_#{inner}"
 #примитивный парсинг
 File.open(inner, "r:utf-8") do |f|  
   table=CSV.parse(f, headers: true)
-  #table.by_col[1]
-  #table.by_row[0]
+  #table.by_col[0]
+  
   #table[0]["№"]
   #table[0]["марка"]
   #table.empty?
   #table.size
   #table.length()
-  row=[]
-  table.headers.each do|w|
-    # привести нименнования к стандартным
-    w=w.strip.downcase
-    #настройку regex целесобрзно произволить по отдельности
-    w=w.gsub(/№/,"№")
-    w=w.gsub(/^мар.*/,"Марка")
-    w=w.gsub(/^разм.*/,"Размер")
-    w=w.gsub(/^исп.*/,"Исполнение")
-    w=w.gsub(/^напр.*/,"Напряжение")
-    w=w.gsub(/^тех.*/,"ТУ")
-    w=w.gsub(/^гост.*/,"ГОСТ")
-    w=w.gsub(/^цвет.*/,"Цвет")
-    row<<w
+  head=table.headers
+  head_data=[]
+  normalize_headers_names head,head_data  
+  add_header_data_outer outer,head_data
+  table.by_row.each do|row|
+    add_data_outer  outer,row
   end
-  data=row
-  add_data_outer outer,data
 end
 
 
