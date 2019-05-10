32a157a4c6b8c6d93745b24395f499fb60fa91f9
kv-2 <str_token@mail.ru>
2019-05-10 17:05:22 +0300

    add data folder

--- parser.rb
+++ parser.rb
@@ -10,19 +10,21 @@ loger="loger_#{inner}"
 #примитивный парсинг
 File.open(inner, "r:utf-8") do |f|  
   table=CSV.parse(f, headers: true)
-  #table.by_col[0]
-  
+  #table.by_col[0]  
   #table[0]["№"]
   #table[0]["марка"]
   #table.empty?
   #table.size
   #table.length()
+  #получить заголовки
   head=table.headers
   head_data=[]
   # нормализация заголовков
   normalize_headers_names head,head_data  
   #добавить заголовки в новую таблицу
   add_header_data_outer outer,head_data
+  # нормализация данных, перебор списков в папке data
+
   #добавить даные в новую таблицу
   add_data_outer  outer,table
 end
