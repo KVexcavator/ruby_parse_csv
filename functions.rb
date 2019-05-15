8002fe4cff411e4edd5781f80057700fe9b21a26
kv-2 <str_token@mail.ru>
2019-05-15 20:02:05 +0300

    add function prepare_word

--- functions.rb
+++ functions.rb
@@ -20,6 +20,19 @@ def add_data_outer  outer,table
   return o
 end
 
+#функция подготовки данных к поиску по рег выпажению
+# принимает имя,слово,набор символов для затирания, возвращает обрубок
+def prepare_word name,word,exeptions
+  #приготовить слово
+  unless word.nil?
+    word=word.strip.downcase.tr(exeptions,'')
+    else
+      word='misstake'
+      log.error("Not find! #{name}  --  #{word}!")
+    end
+  return word
+end
+
 # функция приводит неупорядоченные заголовки к стандартным
 # принимпет входящие заголовкм head, и пустой массив head_data для упорядоченных заголоавков
 # возвращает новый ряд с заголовками
@@ -27,18 +40,11 @@ def normalize_headers_names head,head_data,log
   # получить массив образцов строк для распознования и подстановки
   load "data/list_headers.rb"
   pattern_array=get_headers  
-  # привести нименнования к стандартным
+  # привести нименнования к стандартным  
   head.each do |word|
-    #придготовить слово
-    unless word.nil?
-    stub=word.strip.downcase.tr('^а-я№','')
-    else
-      stub='misstake'
-      log.error("Not inter name!")
-    end
-    #найти соответствие
+    # опознать принятые данные  
     pattern_array.each do |array|
-      w=array.grep  (/^#{stub}\w*/i) 
+      w=array.grep  (/^#{prepare_word "headers",word,'^а-я№'}\w*/i) 
       head_data<<array[0] unless w.empty?
       head_data.uniq!
     end    
@@ -46,8 +52,15 @@ def normalize_headers_names head,head_data,log
 
   # функция обрабатывает данные в еолонке Цвет
   def normalize_colors table
-    p table.by_col[0]
-    p table[0]["Цвет"]
+    load "data/list_colors.rb"
+    pattern_array=get_colors
+    pattern_array.each do |array|
+    #p array[0]
+    pattern="/#{array}/i"
+    for word in table["Цвет"]
+      #p word
+    end
+    end
   end
 
 end
